d402f6dddbf9d31be70590a9817036c7536bd85e
Gabriel <jgabrielfd0@gmail.com>
2023-03-20 18:29:23 -0300

    Programa proposto no exercício 6 que imprime o meu nome e para qual time eu torco

---
+++ Question6.java
@@ -0,0 +1,6 @@
+public class Question6{
+	public static void main(String[] args) {
+		System.out.println("José Gabriel");
+		System.out.println("Eu não torco pra nenhum time, porem meu colega torce para o Palmeiras");
+	}
+}
